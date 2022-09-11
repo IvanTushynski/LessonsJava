4200f3d26676c35b6cee8bed80d11fccbb24bdf6
Ivan Tushynski <tushinski.ivan@gmail.com>
2022-09-11 12:55:50 +0300

    add String teset and testTwo

--- src/com/homeworks/Strings.java
+++ src/com/homeworks/Strings.java
@@ -4,12 +4,13 @@ public class Strings {
     public static void main(String[] args) {
         int x = 4;
         String test = "Test";
+        String testTwo = "TestTwo";
         String myStringOne = "Hello";
         String myStringTwo = "World";
         String myStringThree = " ";
         System.out.println(myStringOne + myStringThree + myStringTwo);
         System.out.println("Hello" + " " + "World");
         System.out.println("My number is" + " " + x);
-        System.out.println(test);
+        System.out.println(test + testTwo);
     }
 }
