44a0db35d9d9841ee5e9e751174cfb8657a946da
Ivan Tushynski <tushinski.ivan@gmail.com>
2022-09-11 21:56:00 +0300

    add String teset and testTwo

--- src/com/homeworks/For.java
+++ src/com/homeworks/For.java
@@ -2,6 +2,12 @@ package com.homeworks;
 
 public class For {
     public static void main(String[] args) {
+        for (int i = 0 ; i <= 10 ; i = i + 2 ){
+            System.out.println("hello" + " " + i);
+        }
 
+        for ( int cola = 0 ; cola <= 20%18 ; cola++ ) {
+            System.out.println(cola);
+        }
     }
 }
