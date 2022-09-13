2b24bc6968c86e41ad9012742cb6c223e8eec837
Ivan Tushynski <tushinski.ivan@gmail.com>
2022-09-13 11:24:29 +0300

    reload

--- src/com/homeworks/BreakContinue.java
+++ src/com/homeworks/BreakContinue.java
@@ -2,6 +2,21 @@ package com.homeworks;
 
 public class BreakContinue {
     public static void main(String[] args) {
+        int a = 0;
+        while (true) {
+            if (a == 20){
+                break;
+            }
+            System.out.println(a);
+            a++;
+        }
+        System.out.println("Цикл окончен");
 
+        for (int i = 0; i <= 20; i++) {
+            if (i%2 == 0) {
+                continue;
+            }
+            System.out.println("Нечетное число" + " " + i);
+        }
     }
 }
---
+++ src/com/homeworks/Switch.java
@@ -0,0 +1,24 @@
+package com.homeworks;
+
+import java.util.Scanner;
+
+public class Switch {
+    public static void main(String[] args) {
+        Scanner sca = new Scanner(System.in);
+        System.out.println("Угадай цифру:");
+        int age = sca.nextInt();
+        switch (age) {
+            case 0 :
+                System.out.println("Угадана Цифра 0");
+                break;
+            case  7 :
+                System.out.println("Угадана Цифра 7");
+                break;
+            case  18 :
+                System.out.println("Угадана Цифра 18");
+                break;
+            default:
+                System.out.println("Это не та цифра");
+        }
+    }
+}
