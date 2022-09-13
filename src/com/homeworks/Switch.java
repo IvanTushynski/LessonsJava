4bad40ca4688b47f21e873370c4371c66bc4ae11
Ivan Tushynski <tushinski.ivan@gmail.com>
2022-09-13 11:37:51 +0300

    reload

--- src/com/homeworks/Switch.java
+++ src/com/homeworks/Switch.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Switch {
     public static void main(String[] args) {
         Scanner sca = new Scanner(System.in);
-        System.out.println("Угадай цифру:");
+        /*System.out.println("Угадай цифру:");
         int age = sca.nextInt();
         switch (age) {
             case 0 :
@@ -17,8 +17,21 @@ public class Switch {
             case  18 :
                 System.out.println("Угадана Цифра 18");
                 break;
+            default: System.out.println("Это не та цифра");
+        }*/
+
+        System.out.println("What is Milk?");
+        String milk = sca.nextLine();
+        switch (milk) {
+            case "Cow" :
+                System.out.println("Cow Milk");
+                break;
+            case "goat" :
+                System.out.println("Goat Milk");
+                break;
             default:
-                System.out.println("Это не та цифра");
+                System.out.println("Another Milk");
         }
+
     }
 }
