d0d4d84f08432a2971cc71eb4402fdb7866125b3
IvanTushinski <tushinski.ivan@gmail.com>
2022-09-22 12:54:29 +0300

    update

---
+++ src/com/homeworks/Classes_And_Objects.java
@@ -0,0 +1,12 @@
+package com.homeworks;
+
+public class Classes_And_Objects {
+    //может быть только один
+}
+
+class test1 {
+    //может быть сколько угодно
+}
+class test2 {
+    //может быть сколько угодно
+}
