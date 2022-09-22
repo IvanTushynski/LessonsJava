89056791847f999abc2b699fee8e8cf6613ecf9a
Ivan Tushynski <tushinski.ivan@gmail.com>
2022-09-22 14:26:03 +0300

    upload

--- src/com/homeworks/Classes_And_Objects.java
+++ src/com/homeworks/Classes_And_Objects.java
@@ -4,13 +4,24 @@ public class Classes_And_Objects {
     //может быть только один
     //имя класса должно соответствовать названию файла
     public static void main(String[] args) {
+        Person ivan = new Person();
+        Person dasha = new Person();
 
+        ivan.name = "Ivan";
+        ivan.age = 20;
+        dasha.name = "Dasha";
+        dasha.age = 30;
+        //System.out.println(ivan.name + " ----- " + dasha.name);
+        //System.out.println(ivan.age + " ----- " + dasha.age);
     }
 }
 
-class test1 {
-    //может быть сколько угодно
-}
-class test2 {
-    //может быть сколько угодно
+class Person {
+    //у класса могут быть. данные(поля)
+    String name;
+    int age;
+    //действия которые он может совершать(методы)
+    void Speak() {
+        System.out.println("Меня зовут " + name + ", " + "Мне " + age + " Лет");
+    }
 }
