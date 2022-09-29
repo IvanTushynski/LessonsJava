6a358d3c38b20463d8c3accc1a40805491d83558
IvanTushinski <tushinski.ivan@gmail.com>
2022-09-29 19:59:01 +0300

    update

--- src/com/homeworks/Getters_And_Setters.java
+++ src/com/homeworks/Getters_And_Setters.java
@@ -3,14 +3,14 @@ package com.homeworks;
 public class Getters_And_Setters {
     public static void main(String[] args) {
         People person = new People();
-        //person.name = "Dasha";
-        //person.years = 33;
-        //System.out.println(person.name + " " + person.years);
+        person.name = "Dasha";
+        person.years = 33;
+        System.out.println(person.name + " " + person.years);
     }
 }
 
 class People {
-    private String name;
-    private int years;
+    String name;
+    int years;
 
 }
---
+++ src/com/homeworks/Random.java
@@ -0,0 +1,9 @@
+package com.homeworks;
+
+public class Random {
+    public static void main(String[] args) {
+        //рандомное число
+        int a = 2 + (int) (Math.random()*10);
+        System.out.println(a);
+    }
+}
