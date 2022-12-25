88979f07db47bede52accd3be0c41440161f3eb0
Ivan Tushynski <tushinski.ivan@gmail.com>
2022-12-25 17:40:21 +0300

    upload

--- src/com/homeworks/Getters_And_Setters.java
+++ src/com/homeworks/Getters_And_Setters.java
@@ -6,9 +6,11 @@ public class Getters_And_Setters {
     public static void main(String[] args) {
         Cars cars = new Cars();
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter Name Of Car!");
         int nameCar = scanner.nextInt();
         if (nameCar == 1){
             cars.setName(1);
+            cars.setPrice(100);
         } else if (nameCar == 2) {
             cars.setName(2);
         }else {
@@ -24,11 +26,20 @@ class Cars {
     private short mileage;
 
     public void setName(int name) {
-        if (name <= 1) {
+        if (name <= 10) {
             this.name = name;
-            System.out.println(name);
+            System.out.println(name + " NAME");
         } else {
             System.out.println("Name Very Long!");
+        }
+    }
+
+    public void setPrice(int price) {
+        if (price <= 10000) {
+            this.price = price;
+            System.out.println(price + " PRICE");
+        } else {
+            System.out.println("Price Very Long!");
         }
     }
 
