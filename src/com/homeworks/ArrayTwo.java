b80478d366ae6461511f94f5154016d824c856e2
Ivan Tushynski <tushinski.ivan@gmail.com>
2022-12-30 12:15:47 +0300

    upload

--- src/com/homeworks/ArrayTwo.java
+++ src/com/homeworks/ArrayTwo.java
@@ -10,6 +10,27 @@ public class ArrayTwo {
         }
 
         String[][] array = new String[][]{{"a", "b", "c", "d", "e", "f", "g", "h", "j", "k", " ", "!"}, {"l", "m", "n", "o", "p", "q", "r", "s", "t", "v", "u", "w"}};
-        System.out.print(array[0][9] + array[1][10] + array[1][6] + array[1][6] + array[1][6] + array[1][6] + array[1][11] + array[0][0] + array[0][11]);
+        System.out.print(array[0][9] + array[1][10] + array[1][6] + array[1][6] + array[1][6] + array[1][6] + array[1][11] + array[0][0] + array[0][11] + "\n");
+
+        ArrayNew arrayNew = new ArrayNew();
+        arrayNew.arrayTwo();
+    }
+}
+
+class ArrayNew{
+    public void arrayTwo(){
+        int[][] nums = new int[][]
+                {
+                        {1, 2, 3},
+                        {4, 5, 6},
+                        {7, 8, 9}
+                };
+        for (int i = 0; i < nums.length; i++){
+            for(int j=0; j < nums[i].length; j++){
+
+                System.out.printf("%d ", nums[i][j]);
+            }
+            System.out.println();
+        }
     }
 }
